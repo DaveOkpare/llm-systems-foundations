5a135f56fc7444a53424df0440f50acd2e709764
daveokpare <okparedave@gmail.com>
2026-03-05 20:47:55 +0100

    feat: add function to create dataset from sequence

---
+++ word-segmentation/main.py
@@ -0,0 +1,34 @@
+PAD_TOKEN = "[PAD]"
+
+char_map = {v: i for i, v in enumerate(list("abcdefghijklmnopqrstuvwxyz"), start=1)}
+char_map[PAD_TOKEN] = 0
+
+
+def create_dataset(seq: str, stride: int = 5) -> list:
+    dataset = []
+    clean_seq = "".join(seq.split())
+    seq_len = len(clean_seq)
+    offset = stride // 2
+    ptr = 0
+
+    for i in range(seq_len):
+        start_idx = max(i - offset, 0)
+        end_idx = i + offset + 1
+        pad_left = PAD_TOKEN * max(offset - i, 0)
+        pad_right = PAD_TOKEN * max(end_idx - seq_len, 0)
+
+        substr = pad_left + clean_seq[start_idx:end_idx] + pad_right
+        while ptr < len(seq) and seq[ptr] == " ":
+            ptr += 1
+
+        target = 0
+        if ptr + 1 < len(seq) and seq[ptr + 1] == " ":
+            target = 1
+
+        ptr += 1
+        dataset.append((substr, target))
+    return dataset
+
+
+if __name__ == "__main__":
+    print(create_dataset("i like ai"))
