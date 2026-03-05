3a0a9251ea12fb6b3f4f92737b97aba0f57ce398
daveokpare <okparedave@gmail.com>
2026-03-05 21:50:07 +0100

    feat: add function to vectorize sequences

--- word-segmentation/main.py
+++ word-segmentation/main.py
@@ -1,6 +1,12 @@
+import re
+
+import numpy as np
+
+ALPHABET = list("abcdefghijklmnopqrstuvwxyz")
+VOWELS = list("aeiou")
 PAD_TOKEN = "[PAD]"
 
-char_map = {v: i for i, v in enumerate(list("abcdefghijklmnopqrstuvwxyz"), start=1)}
+char_map = {v: i for i, v in enumerate(ALPHABET, start=1)}
 char_map[PAD_TOKEN] = 0
 
 
@@ -28,6 +34,20 @@ def create_dataset(seq: str, stride: int = 5) -> list:
         ptr += 1
         dataset.append((substr, target))
     return dataset
+
+
+def transform_to_tensor(seq: str):
+    vectors = []
+    _seq = re.findall(r"\[PAD\]|[a-z]", seq)
+
+    for char in _seq:
+        vec = np.zeros(27)
+        idx = char_map[char]
+        vec[idx] = 1
+        vec[-1] = 1 if char in VOWELS else 0
+        vectors.extend(vec.data)
+
+    return vectors
 
 
 if __name__ == "__main__":
