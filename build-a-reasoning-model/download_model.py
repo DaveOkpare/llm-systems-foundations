806e4865d7514db30069d7b8bc24d05b838a0cae
daveokpare <okparedave@gmail.com>
2026-05-27 20:09:52 +0100

    refactor: update the model path and update .gitignore to ignore the path

--- build-a-reasoning-model/download_model.py
+++ build-a-reasoning-model/download_model.py
@@ -1,5 +1,9 @@
 from reasoning_from_scratch.qwen3 import download_qwen3_small
 
 if __name__ == "__main__":
-    download_qwen3_small(kind="base", tokenizer_only=True, out_dir="qwen3")
-    download_qwen3_small(kind="base", tokenizer_only=False, out_dir="qwen3")
+    download_qwen3_small(
+        kind="base", tokenizer_only=True, out_dir="build-a-reasoning-model/qwen3"
+    )
+    download_qwen3_small(
+        kind="base", tokenizer_only=False, out_dir="build-a-reasoning-model/qwen3"
+    )
